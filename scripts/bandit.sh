cca19aa1a8c1e483a920d91500b7cf4973ccf63c
Mustaqiim Muhar <mmuhar@zendesk.com>
2017-06-29 12:31:27 +0800

    Script changes (#3)
    * Choose local .bandit config file
    
    * Check for .bandit in curr dir
    
    * Removed excluded path within -x option

--- scripts/bandit.sh
+++ scripts/bandit.sh
@@ -6,7 +6,7 @@ echo 'Installing bandit'
 pip install bandit
 
 echo 'Running Bandit tests'
-bandit -r --ini .bandit -f json -x bandit/ -o banditResult.json .
+bandit -r --ini .bandit -f json -o banditResult.json .
 
 echo 'Results:'
 python bandit/banditParser.py -o banditResult.json -i bandit.ignore
