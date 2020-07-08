e59cd4ba1f32645b1e00c21a30c83f0bb7009113
Savankumar Gudaas <sgudaas@zendesk.com>
2020-07-08 19:18:24 +0800

    Use pip for backward compatiablity

--- scripts/bandit.sh
+++ scripts/bandit.sh
@@ -4,7 +4,12 @@ set -v
 
 echo 'Installing bandit'
 #pip install --user bandit
-pip3 install bandit
+if [ -x "$(command -v pip3)" ]; then
+    pip3 install bandit
+else
+    pip install --user bandit
+fi
+
 
 echo 'Running Bandit tests'
 bandit -r --ini .bandit -f json -o banditResult.json .
